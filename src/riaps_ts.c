33206aef2bb2524bef5023c98d1adab920ec035b
Peter Volgyesi <peter.volgyesi@vanderbilt.edu>
2016-12-14 14:53:53 -0600

    wip

--- src/riaps_ts.c
+++ src/riaps_ts.c
@@ -18,7 +18,22 @@ int riaps_ts_gettime(struct riaps_ts_timespec *res)
 
 int riaps_ts_sleep(int flags, const struct riaps_ts_timespec *request)
 {
-    return -1;
+    struct timespec cn_req;
+    int cn_flags;
+
+    if (flags ==  RIAPS_TS_ABSTIME) {
+        cn_flags = TIMER_ABSTIME;
+    }
+    else if (flags == RIAPS_TS_RELTIME) {
+        cn_flags = 0;
+    }
+    else {
+        return -1;
+    }
+
+    cn_req.tv_sec = request->tv_sec;
+    cn_req.tv_nsec = request->tv_nsec;
+    return clock_nanosleep(CLOCK_REALTIME, cn_flags, &cn_req, NULL);
 }
 
 
--- src/test.c
+++ src/test.c
@@ -6,7 +6,15 @@
 int main(int argc, const char* argv[])
 {
     struct riaps_ts_timespec now;
-    riaps_ts_gettime(&now);
-    printf("NOW: %ld secs\n", now.tv_sec);
+    struct riaps_ts_timespec snooze;
+
+    snooze.tv_sec = 1;
+    snooze.tv_nsec = 0;
+    
+    for(;;) {
+        riaps_ts_gettime(&now);
+        printf("NOW: %lld.%9ld secs\n", now.tv_sec, now.tv_nsec);
+        riaps_ts_sleep(RIAPS_TS_RELTIME, &snooze);
+    }
     return 0;
 }
