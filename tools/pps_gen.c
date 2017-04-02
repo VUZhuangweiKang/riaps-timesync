5257ea670c426bdb720647b3bc0dd6dd2baeadfc
Peter Volgyesi <peter.volgyesi@vanderbilt.edu>
2017-04-02 16:08:03 +0000

    GPIO benchmarking

--- tools/pps_gen.c
+++ tools/pps_gen.c
@@ -1,10 +1,11 @@
 /*
-  ppm-gen - generate a PPM signal on a GPIO
+  pps-gen - generate a PPS signal on a GPIO
 
-  The PPM signal is aligned to the second boundary (system clock).
-  The PPM signal is generated from software for testing:
+  The PPS signal is aligned to the second boundary (system clock).
+  The PPS signal is generated from software for testing:
     - global timesync
-    - task release jitter
+    - task release jitter (although, the current implementation
+      tries to minimize this effect
 
   Copyright 2017, Peter Volgyesi, Vanderbilt University
 */
@@ -17,7 +18,7 @@
 #include <libsoc_gpio.h>
 
 // P8_19 = 22 (EHRPWM2A)
-#define PPM_OUTPUT  22
+#define PPS_OUTPUT  22
 
 #define BUSY_WAIT_INTERVAL  1000000L // nanoseconds
 
@@ -35,17 +36,19 @@ void setup_scheduler()
 
 int main(int argc, char* argv[])
 {
-  gpio *ppm_output;
+  gpio *pps_output;
   struct timespec t1, t2;
+  struct timespec dbg[5];
+  int idbg;
 
-  ppm_output = libsoc_gpio_request(PPM_OUTPUT, LS_SHARED);
-  if (!ppm_output) {
+  pps_output = libsoc_gpio_request(PPS_OUTPUT, LS_SHARED);
+  if (!pps_output) {
     perror("unable to request gpio pin:");
     exit(-1);
   }
-  libsoc_gpio_set_direction(ppm_output, OUTPUT);
+  libsoc_gpio_set_direction(pps_output, OUTPUT);
 
-  if (libsoc_gpio_get_direction(ppm_output) != OUTPUT)
+  if (libsoc_gpio_get_direction(pps_output) != OUTPUT)
   {
     perror("unable to set output direction:");
     exit(-1);
@@ -72,22 +75,33 @@ int main(int argc, char* argv[])
       exit(-1);
     }
 
-    // busy wait for increased accuracy
+    // busy wait for increased accuracy (approx 2us accuracy on BBB)
+    idbg = 0;
     do {
       if (clock_gettime(CLOCK_REALTIME, &t2)) {
         perror("clock_gettime failed:");
         exit(-1);
       }
+      dbg[idbg] = t2; idbg = (idbg + 1) % 5;
     } while (t2.tv_sec <= t1.tv_sec);
 
-    libsoc_gpio_set_level(ppm_output, HIGH);
+    libsoc_gpio_set_level(pps_output, HIGH);
     //usleep(10);
-    libsoc_gpio_set_level(ppm_output, LOW);
-    printf("%lld.%.9ld\n", (long long)t2.tv_sec, t2.tv_nsec); // fflush(stdout);
+    libsoc_gpio_set_level(pps_output, LOW);
+
+    //printf("%lld.%.9ld\n", (long long)t2.tv_sec, t2.tv_nsec); // fflush(stdout);
+    printf("%ld ns\n", t2.tv_nsec); // fflush(stdout);
+    for (int i = 0; i < 5; i++) {
+      long nsec = dbg[(idbg + i) % 5].tv_nsec;
+      if (nsec > 500000000) {
+        nsec = 1000000000 - nsec;
+      }
+      printf("\t%ld ns\n", nsec); 
+    }
   }
 
-  if (ppm_output)
+  if (pps_output)
   {
-    libsoc_gpio_free(ppm_output);
+    libsoc_gpio_free(pps_output);
   }
 }
