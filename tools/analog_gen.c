303e67072d96b2a905c02e21cd318ebdf98f318b
Peter Volgyesi <peter.volgyesi@vanderbilt.edu>
2017-04-24 19:42:10 +0000

    using absolute time for better sampling

--- tools/analog_gen.c
+++ tools/analog_gen.c
@@ -25,8 +25,8 @@
 #define SIGNAL_FREQ 60 // Hz
 #define PWM_PERIOD 4000 // nanoseconds
 
-
-#define SAMPLING_INTERVAL (1000000000L / SAMPLING_RATE) // nanoseconds
+#define NS_PER_SEC 1000000000L
+#define SAMPLING_INTERVAL (NS_PER_SEC / SAMPLING_RATE) // nanoseconds
 #define DPHASE (2 * M_PI * SIGNAL_FREQ / SAMPLING_RATE)
 
 // ANALOG (PWM) OUTPUT: P9_14 = (EHRPWM1A)
@@ -49,7 +49,7 @@ void scheduler_setup()
 int main(int argc, char* argv[])
 {
   double phase = 0.0;
-  struct timespec t_wait = {0L, SAMPLING_INTERVAL};
+  struct timespec t_wait;
   pwm *pwm_output;
 
   // libsoc_set_debug(1);
@@ -86,22 +86,22 @@ int main(int argc, char* argv[])
   }
 
 
+  if (clock_gettime(CLOCK_REALTIME, &t_wait)) {
+    perror("clock_gettime failed:");
+    exit(-1);
+  }
   while (1) {
-    libsoc_pwm_set_duty_cycle(pwm_output, PWM_PERIOD * (1.0 + sin(phase)) / 2.0 );
-    phase += DPHASE;
-
-    if (clock_nanosleep(CLOCK_REALTIME, 0, &t_wait, NULL)) {
+    t_wait.tv_nsec += SAMPLING_INTERVAL;
+    if (t_wait.tv_nsec >= NS_PER_SEC) {
+      t_wait.tv_sec++;
+      t_wait.tv_nsec -= NS_PER_SEC;
+    }
+    if (clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &t_wait, NULL)) {
       perror("clock_nanosleep failed:");
       exit(-1);
     }
 
-    // if (clock_gettime(CLOCK_REALTIME, &t)) {
-    //    perror("clock_gettime failed:");
-    //    exit(-1);
-    // }
-
-    // printf("%lld.%.9ld [%ld...%ld ns]\n",
-    //          (long long)t.tv_sec, t.tv_nsec,
-    //           ns_diff(&t, &t_epoch), ns_diff(&t_asserted, &t_epoch));
+    libsoc_pwm_set_duty_cycle(pwm_output, PWM_PERIOD * (1.0 + sin(phase)) / 2.0 );
+    phase += DPHASE;
   }
 }
