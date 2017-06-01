bab3354f40d0775538137d50bbf704ab90b4192b
Peter Volgyesi <peter.volgyesi@vanderbilt.edu>
2017-06-01 20:44:02 +0000

    skip python packages for now

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='riaps_ts',
-    version='1.0',
+    name='riaps_timesync',
+    version='1.1.1',
     py_modules=['riaps_ts']
 )
--- version.sh
+++ version.sh
@@ -1,2 +1,1 @@
-export externalsversion="0.4.4" 
 export timesyncversion="1.1.1"
